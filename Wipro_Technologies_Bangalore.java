f3eb00e65ac21c3411d5845d19694b4f66c27b14
udaykumarsamala <58910376+udaykumarsamala@users.noreply.github.com>
2020-06-24 10:10:04 +0530

    Add files via upload

---
+++ Wipro_Technologies_Bangalore.java
@@ -0,0 +1,10 @@
+package Command_line;
+
+public class Wipro_Technologies_Bangalore {
+
+	public static void main(String args[]) {
+		
+   System.out.println(args[0]+"Technologies"+args[1]);
+	}
+
+}
